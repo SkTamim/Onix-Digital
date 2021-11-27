874a3070931a8a34377d872d88ae77d583d0023f
Sk Tamim <sktamim225@gmail.com>
2021-11-27 15:42:57 +0530

    Added all assets and Completed main navigation section with responsiveness

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+// Main JS file for Onix
+
+// Fixed NavBar Animation
+let navBar = document.querySelector("nav");
+window.onscroll = () => {
+	if (window.scrollY > 20) {
+		navBar.style.height = "80px";
+		navBar.classList.add("nav-shadow");
+		navBar.classList.replace("py-4", "py-3");
+		navBar.style.backgroundColor = "var(--white)";
+	} else {
+		navBar.style.height = "100px";
+		navBar.classList.remove("nav-shadow");
+		navBar.classList.replace("py-3", "py-4");
+		navBar.style.backgroundColor = "var(--grey)";
+	}
+};
+
+// For menu Toggle icon
+const toggle_btn = document.querySelector(".menu-trigger");
+toggle_btn.addEventListener("click", function () {
+	toggle_btn.classList.toggle("active");
+});
